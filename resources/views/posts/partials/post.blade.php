839ec123daf02b4f700ec65cda8dbf4d60f35c7f
mazharul95 <mipiyas07@gmail.com>
2022-06-28 00:19:13 +0600

    Epi 09 Sec 09 Partial Templates (Including Templates)

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,8 +1,7 @@
 {{-- @break($key = 2) --}}
 {{-- @continue($key = 1) --}}
-@if($loop->even)
-<div>{{ $key }}.{{ $post['title'] }}</div>
+  @if ($loop->even)
+    <div>{{ $key }}.{{ $post['title'] }}</div>
 @else
-<div style="background-color: silver">{{ $key }}.{{ $  post['title'] }}</div> 
+    <div style="background-color: silver">{{ $key }}.{{ $post['title'] }}</div>
 @endif
-  
