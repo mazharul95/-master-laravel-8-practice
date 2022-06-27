92a78d3f7fd506b75cb4b289c42349eea838b41f
mazharul95 <mipiyas07@gmail.com>
2022-06-28 00:37:26 +0600

    Epi 09 Sec 10 Partial Templates in loop

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Blog Posts')
 
 @section('content')
+
+    {{-- @each('posts.partials.post', $posts, 'post') --}}
     @forelse ($posts as $key => $post)
-        @include('posts.partials.post')
+        @include('posts.partials.post', [])
     @empty
         No Posts Found!
     @endforelse
--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,6 +1,6 @@
 {{-- @break($key = 2) --}}
 {{-- @continue($key = 1) --}}
-  @if ($loop->even)
+@if ($loop->even)
     <div>{{ $key }}.{{ $post['title'] }}</div>
 @else
     <div style="background-color: silver">{{ $key }}.{{ $post['title'] }}</div>
