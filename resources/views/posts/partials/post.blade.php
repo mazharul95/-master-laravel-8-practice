06eb921727721d3b764d69f3972af0adc553e9e8
mazharul95 <mipiyas07@gmail.com>
2022-07-04 03:07:58 +0600

    Epi 17 Sec 11 Styling Single Post Page

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,6 +1,5 @@
 <h3><a href="{{ route('posts.show', ['post' => $post->id]) }}"> {{ $post->title }} </a></h3>
 
-
 <div class="mb-3">
     <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
     <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
