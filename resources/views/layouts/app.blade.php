c995b2e5bfbd27b7ffe98ff544588ad236481f5a
mazharul95 <mipiyas07@gmail.com>
2022-07-09 05:25:38 +0600

    Epi 23 Sec 10 Retrieving the currently authenticated user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,12 +29,10 @@
                 <a class="p-2 text-dark" href="{{ route('login') }}">Login</a>
             @else
                 <a class="p-2 text-dark" href="{{ route('logout') }}"
-                
-                    onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-                    >Logout</a>
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout
+                    ({{ Auth::user()->name }})</a>
 
-                <form id="logout-form" action={{ route('logout') }} method="POST" 
-                    style="display: none;"> 
+                <form id="logout-form" action={{ route('logout') }} method="POST" style="display: none;">
                     @csrf
                 </form>
             @endguest
