c5f5a1fef6b29853a2100f3e05b80630a9ba801d
mazharul95 <mipiyas07@gmail.com>
2022-08-06 06:13:38 +0600

    Epi 27 Sec 07 Practical Local Query Scope - most active users last month

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,4 +39,11 @@ class User extends Authenticatable
         return $query->withCount('blogPosts')->orderBy('blog_posts_count', 'desc');
     }
 
+    public function scopeWithMostBlogPostsLastMonth(Builder $query)
+    {
+        return $query->withCount(['blogPosts' => function (Builder $query) {
+            $query->whereBetween(static::CREATED_AT, [now()->subMonth(1), now()]);
+        }])->having('blog_posts_count', '>=', 2)
+            ->orderBy('blog_posts_count', 'desc');
+    }
 }
